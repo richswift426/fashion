02b92a847b33c3ec4e18145a867c417bdd044b72
devmachine <matheusangel534@gmail.com>
2023-02-18 15:01:39 -0500

    Add image upload slot for users and products

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import { GithubIcon } from "components/icons/GithubIcon";
+import ImageUploading from "react-images-uploading";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [userImages, setUserImages] = useState([]);
+  const maxNumber = 69;
+
+  const onProductsChange = (imageList, addUpdateIndex) => {
+    console.log(imageList, addUpdateIndex);
+    setProducts(imageList);
+  };
+
+  const onUsersChange = (users, addUpdateIndex) => {
+    console.log(users, addUpdateIndex);
+    setUserImages(users);
+  };
+
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
 
   return (
     <>
@@ -25,7 +43,84 @@ export default function Home() {
       <main className="main">
         <h1>Fashion App</h1>
         <div className="content container">
-          <div className="space-y-4"></div>
+          <div className="space-y-4">
+            <div className="products-slot">
+              <div className="products-slot-title">Upload products</div>
+              <ImageUploading
+                multiple
+                value={products}
+                onChange={onProductsChange}
+                maxNumber={maxNumber}
+                dataURLKey="data_url"
+                acceptType={["jpg", "jpeg", "png"]}
+              >
+                {({
+                  imageList,
+                  onImageUpload,
+                  onImageRemoveAll,
+                  onImageUpdate,
+                  onImageRemove,
+                  isDragging,
+                  dragProps,
+                }) => (
+                  <>
+                    <button
+                      style={isDragging ? { color: "red" } : null}
+                      onClick={onImageUpload}
+                      {...dragProps}
+                    >
+                      Click or Drop here
+                    </button>
+                    <div className="uploaded-products">
+                      {products.map((image, index) => (
+                        <div key={index} className="image-item">
+                          <img src={image.data_url} alt="Product image" />
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </ImageUploading>
+            </div>
+            <div className="users-slot">
+              <div className="users-slot-title">Upload user images</div>
+              <ImageUploading
+                multiple
+                value={userImages}
+                onChange={onUsersChange}
+                maxNumber={maxNumber}
+                dataURLKey="data_url"
+                acceptType={["jpg", "jpeg", "png"]}
+              >
+                {({
+                  imageList,
+                  onImageUpload,
+                  onImageRemoveAll,
+                  onImageUpdate,
+                  onImageRemove,
+                  isDragging,
+                  dragProps,
+                }) => (
+                  <>
+                    <button
+                      style={isDragging ? { color: "red" } : null}
+                      onClick={onImageUpload}
+                      {...dragProps}
+                    >
+                      Click or Drop here
+                    </button>
+                    <div className="uploaded-products">
+                      {userImages.map((image, index) => (
+                        <div key={index} className="image-item">
+                          <img src={image.data_url} alt="Product image" />
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </ImageUploading>
+            </div>
+          </div>
 
           {/** Show the images a user has created */}
           {/* <div className="pt-1 space-y-4">
